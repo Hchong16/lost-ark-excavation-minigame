2c72164a02bdccb7a2b1e8e046ccb72e7c513918
hchong.16 <hchong.16@gmail.com>
2023-03-13 23:39:46 -0400

    time based mvp

--- minigame.py
+++ minigame.py
@@ -5,9 +5,9 @@ from time import sleep
 import numpy as np
 import cv2 as cv2
 
-normal_spacebar = cv2.imread("./asset/normal_spacebar.png", 0)
-glow_spacebar = cv2.imread("./asset/glow_spacebar.png", 0)
-minigame_arrow = cv2.imread("./asset/minigame_arrow.png", 0)
+normal_spacebar = cv2.imread("./assets/normal_spacebar.png", 0)
+glow_spacebar = cv2.imread("./assets/glow_spacebar.png", 0)
+minigame_arrow = cv2.imread("./assets/minigame_arrow.png", 0)
 
 def automate_space() -> None:
     keyDown('space')
@@ -15,23 +15,38 @@ def automate_space() -> None:
     keyUp('space')
 
 def search_targets() -> list:
-    bar = screenshot(region=(725, 720, 450, 20))
+    sleep(0.5)
+    bar = screenshot('bar.png', region=(740, 720, 430, 20))
     bar_rgb = np.array(bar)
     bar_gray = cv2.cvtColor(bar_rgb, cv2.COLOR_BGR2GRAY)
 
-    ret, bar_bw = cv2.threshold(bar_gray, 95, 255, cv2.THRESH_BINARY)
+    _, bar_bw = cv2.threshold(bar_gray, 95, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(bar_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     targets = []
     for target in contours:
         if cv2.contourArea(target) >= 100:
-            M = cv2.moments(target)
-            if M['m00'] != 0:
-                cx = int(M['m10']/M['m00'])
-                # cy = int(M['m01']/M['m00']) # We don't care about the y-axis
-            targets.append([cx-15, cx+15])
+            M = cv2.moments(target) # Middle x coord of target
+            targets.append(int(M['m10']/M['m00']))
 
-    return targets
+    targets.sort()
+
+    times = []
+    curr, prev = None, None
+    for idx, target in enumerate(targets):
+        curr = target
+
+        if idx == 0:
+            prev = curr
+            times.append(curr/214)
+        else:
+            times.append((curr - prev)/214)
+            prev = curr
+
+        # print("idx: {}, curr: {}, prev: {}, time: {}".format(idx, curr, prev, times[-1]))
+
+    assert len(times) == 3
+    return times
 
 counter = 3
 targets = []
@@ -51,29 +66,30 @@ while is_pressed('q') == False:
     _, n_confidence, _, max_loc = cv2.minMaxLoc(normal_res)
     _, g_confidence, _, max_loc = cv2.minMaxLoc(glow_res)
 
-    if n_confidence >= 0.8 or g_confidence >= 0.8: 
-        if not targets and skip_first:
-            sleep(1)
-
-        game = screenshot(region=(725, 740, 450, 30))
-        game_rgb = np.array(game)
-        game_gray = cv2.cvtColor(game_rgb, cv2.COLOR_BGR2GRAY)
-
-        if not targets:
+    if n_confidence >= 0.8 or g_confidence >= 0.8:
+        if not targets: 
             targets = search_targets()
+        print(targets)
+        sleep(2.80) # Config value?
+        print("ARROW MOVED")
 
-        arrow_res = cv2.matchTemplate(game_gray, minigame_arrow, cv2.TM_CCOEFF_NORMED)
-        _, arrow_confidence, _, arrow_loc = cv2.minMaxLoc(arrow_res)
-        if arrow_confidence >= 0.8:
-            # Offset middle of arrow to trigger spacebar early. This is to account for delays.
-            middle = arrow_loc[0] + 15
-            for idx, target in enumerate(targets):
-                if middle <= target[1] and middle >= target[0] and counter > 0:
-                    targets.remove(targets[idx])
-                    automate_space()
-                    counter -= 1
-                    print("IN RANGE OF: {} ({})".format(target, middle))
-                    print("PRESSING SPACE")
+        sleep(targets[0])
+        automate_space()
+ 
+        sleep(targets[1])
+        automate_space()
+
+        sleep(targets[2])
+        automate_space()
+        
+        
+
+
+
+
+
+
+
     else:
         counter = 3
         targets = []
