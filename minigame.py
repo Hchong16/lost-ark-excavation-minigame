3e3e2f144532ea68ae077383963dd9dca453961b
hchong.16 <hchong.16@gmail.com>
2023-03-18 18:03:43 -0400

    documentations + clean up

--- minigame.py
+++ minigame.py
@@ -1,36 +1,38 @@
-from pyautogui import screenshot, keyUp, keyDown
-from matplotlib import pyplot  as plt
-from keyboard import is_pressed
-from time import sleep
-import mss 
-import numpy as np
 import cv2 as cv2
+import mss as mss
+import numpy as np
+from keyboard import is_pressed
+from matplotlib import pyplot  as plt
+from pyautogui import keyUp, keyDown
+from time import sleep
 
-normal_spacebar = cv2.imread("./assets/normal_spacebar.png", 0)
-glow_spacebar = cv2.imread("./assets/glow_spacebar.png", 0)
-minigame_arrow = cv2.imread("./assets/minigame_arrow.png", 0)
+MINIGAME_ARROW = cv2.imread("./assets/minigame_arrow.png", 0)
+NORMAL_SACEBAR = cv2.imread("./assets/normal_spacebar.png", 0)
+GLOW_SPACEBAR = cv2.imread("./assets/glow_spacebar.png", 0)
 
 def automate_space() -> None:
     keyDown('space')
     sleep(0.1)
     keyUp('space')
 
-def search_targets() -> list:
-    bar_rgb = np.array(sct.grab({"left": 725, "top": 720, "width": 450, "height": 20}))
-    bar_gray = cv2.cvtColor(bar_rgb, cv2.COLOR_BGR2GRAY)
+def search_targets(offset = 4) -> list:
+    """
+    Search for the target zones within the excavation bar by returning the x-ranges
+    for each individual zones. Add offsets to account for latency between the program and
+    the game. 
+    """
+    excavation_bar_rgb = np.array(sct.grab({"left": 725, "top": 720, "width": 450, "height": 20}))
+    excavation_bar_gray = cv2.cvtColor(excavation_bar_rgb, cv2.COLOR_BGR2GRAY)
 
-    _, bar_bw = cv2.threshold(bar_gray, 70, 255, cv2.THRESH_BINARY)
+    _, bar_bw = cv2.threshold(excavation_bar_gray, 70, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(bar_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     targets = []
     for target in contours:
         if cv2.contourArea(target) >= 100:
             M = cv2.moments(target)
-            cx = int(M['m10']/M['m00'])
-
-            # Value might change based on difficult shovel?
-            offset = 4
-            targets.append([cx-offset, cx+offset])
+            x = int(M['m10']/M['m00'])
+            targets.append([x-offset, x+offset])
 
     return targets
 
@@ -38,16 +40,12 @@ targets = []
 searched = False
 while is_pressed('=') == False:
     with mss.mss() as sct:
-        # To optimize speed, extract specific parts of the screen. The following
-        # assumes the program is on a 1080p + fullscreen application. 
-        img_rgb = np.array(sct.grab({"left": 880, "top": 775, "width": 150, "height": 90}))
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        spacebar_img_rgb = np.array(sct.grab({"left": 880, "top": 775, "width": 150, "height": 90}))
+        spacebar_img_gray = cv2.cvtColor(spacebar_img_rgb, cv2.COLOR_BGR2GRAY)
 
-        # plt.subplot(111), plt.imshow(img_gray)
-        # plt.show()
-
-        normal_res = cv2.matchTemplate(img_gray, normal_spacebar, cv2.TM_CCOEFF_NORMED)
-        glow_res = cv2.matchTemplate(img_gray, glow_spacebar, cv2.TM_CCOEFF_NORMED)
+        # Determine whether the game have started  by searching for the spacebar assets.
+        normal_res = cv2.matchTemplate(spacebar_img_gray, NORMAL_SACEBAR, cv2.TM_CCOEFF_NORMED)
+        glow_res = cv2.matchTemplate(spacebar_img_gray, GLOW_SPACEBAR, cv2.TM_CCOEFF_NORMED)
 
         _, n_confidence, _, max_loc = cv2.minMaxLoc(normal_res)
         _, g_confidence, _, max_loc = cv2.minMaxLoc(glow_res)
@@ -57,23 +55,23 @@ while is_pressed('=') == False:
             if not targets:
                 sleep(4.5)
 
-            game_rgb = np.array(sct.grab({"left": 725, "top": 740, "width": 450, "height": 30}))
-            game_gray = cv2.cvtColor(game_rgb, cv2.COLOR_BGR2GRAY)
+            arrow_rgb = np.array(sct.grab({"left": 725, "top": 740, "width": 450, "height": 30}))
+            arrow_gray = cv2.cvtColor(arrow_rgb, cv2.COLOR_BGR2GRAY)
 
             if not searched:
                 searched = True
                 targets = search_targets()
 
-            arrow_res = cv2.matchTemplate(game_gray, minigame_arrow, cv2.TM_CCOEFF_NORMED)
+            arrow_res = cv2.matchTemplate(arrow_gray, MINIGAME_ARROW, cv2.TM_CCOEFF_NORMED)
             _, arrow_confidence, _, arrow_loc = cv2.minMaxLoc(arrow_res)
             if arrow_confidence >= 0.75:
-                # Offset middle of arrow to trigger spacebar early. This is to account for delays.
-                middle = arrow_loc[0] + 14
+                # Offset to the middle of arrow coordinate.
+                location = arrow_loc[0] + 14
                 for idx, target in enumerate(targets):
-                    if middle <= target[1] and middle >= target[0]:
+                    if location <= target[1] and location >= target[0]:
                         targets.remove(targets[idx])
                         automate_space()
-                        print("IN RANGE OF: {} ({})".format(target, middle))
+                        print("IN RANGE OF: {} ({})".format(target, location))
                         print("PRESSING SPACE\n")
         else:
             targets = []
