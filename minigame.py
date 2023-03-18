8b7075d879d14899e0d81c1e6aeb9b10bae024f3
hchong.16 <hchong.16@gmail.com>
2023-03-18 16:58:47 -0400

    offset fixes

--- minigame.py
+++ minigame.py
@@ -19,27 +19,27 @@ def search_targets() -> list:
     bar_rgb = np.array(sct.grab({"left": 725, "top": 720, "width": 450, "height": 20}))
     bar_gray = cv2.cvtColor(bar_rgb, cv2.COLOR_BGR2GRAY)
 
-    _, bar_bw = cv2.threshold(bar_gray, 50, 255, cv2.THRESH_BINARY)
+    _, bar_bw = cv2.threshold(bar_gray, 70, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(bar_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     targets = []
     for target in contours:
         if cv2.contourArea(target) >= 100:
             M = cv2.moments(target)
-            if M['m00'] != 0:
-                cx = int(M['m10']/M['m00'])
-                # cy = int(M['m01']/M['m00']) # We don't care about the y-axis
-            targets.append([cx-5, cx+5])
+            cx = int(M['m10']/M['m00'])
+
+            # Value might change based on difficult shovel?
+            offset = 4
+            targets.append([cx-offset, cx+offset])
 
     return targets
 
 targets = []
+searched = False
 while is_pressed('=') == False:
     with mss.mss() as sct:
         # To optimize speed, extract specific parts of the screen. The following
         # assumes the program is on a 1080p + fullscreen application. 
-
-        # Tested with minigame difficulty 2+ decreased shovel.
         img_rgb = np.array(sct.grab({"left": 880, "top": 775, "width": 150, "height": 90}))
         img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
@@ -53,17 +53,22 @@ while is_pressed('=') == False:
         _, g_confidence, _, max_loc = cv2.minMaxLoc(glow_res)
 
         if n_confidence >= 0.8 or g_confidence >= 0.8:
+            # Let the game load before we do anything
+            if not targets:
+                sleep(4.5)
+
             game_rgb = np.array(sct.grab({"left": 725, "top": 740, "width": 450, "height": 30}))
             game_gray = cv2.cvtColor(game_rgb, cv2.COLOR_BGR2GRAY)
 
-            if not targets:
+            if not searched:
+                searched = True
                 targets = search_targets()
 
             arrow_res = cv2.matchTemplate(game_gray, minigame_arrow, cv2.TM_CCOEFF_NORMED)
             _, arrow_confidence, _, arrow_loc = cv2.minMaxLoc(arrow_res)
             if arrow_confidence >= 0.75:
                 # Offset middle of arrow to trigger spacebar early. This is to account for delays.
-                middle = arrow_loc[0] + 15
+                middle = arrow_loc[0] + 14
                 for idx, target in enumerate(targets):
                     if middle <= target[1] and middle >= target[0]:
                         targets.remove(targets[idx])
@@ -72,3 +77,4 @@ while is_pressed('=') == False:
                         print("PRESSING SPACE\n")
         else:
             targets = []
+            searched = False
